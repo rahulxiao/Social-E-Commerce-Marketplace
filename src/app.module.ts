beda08d9c3a3ad10d6269652f16816769896c9fb
rAHUL <rahulbiswasaiub@gmail.com>
2025-07-23 00:39:55 +0600

    xV0.6

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AdminController } from "./admin/admin.controller";
 import { AdminService } from "./admin/admin.services";
 import { UserController } from "./user/user.controller";
 import { UserService } from "./user/user.services";
+import { ProductController } from './admin/product.controller';
+import { ProductService } from './admin/product.service';
 
 @Module({
   imports: [],
-  controllers: [AdminController,UserController],
-  providers: [AdminService, UserService],
+  controllers: [AdminController,UserController, ProductController],
+  providers: [AdminService, UserService, ProductService],
 })
 export class AppModule {}
--- src/user/user.services.ts
+++ src/user/user.services.ts
@@ -5,7 +5,6 @@ export class UserService {
     getUserInfo() {
         return { message: "User information" };
     }
-
     createUser() {
         return { message: "User created successfully" };
     }
