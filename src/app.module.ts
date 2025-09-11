ac52109830bd2d5345746e7bb88ccbd6051229da
rAHUL <rahulbiswasaiub@gmail.com>
2025-09-11 12:20:55 +0600

    x2.7

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,8 @@ import { ProductModule } from './product/product.module';
 import { OrderModule } from './order/order.module';
 import { CartModule } from './cart/cart.module';
 import { AdminModule } from './admin/admin.module';
-
+import { AuthModule } from './auth/auth.module';
 import { BuyerModule } from './buyer/buyer.module';
-
 import { SellerModule } from './seller/seller.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
@@ -19,8 +18,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       password: 'rahulxiao',
       database: 'trendora',
       autoLoadEntities: true,
-      synchronize: true,
+      synchronize: false,
     }),
+    AuthModule,
     ProductModule,
     CartModule,
     OrderModule,
